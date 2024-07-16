bfd85a89bdf8a2136f6d14de626e1dc9481f3f93
KartikKoli <kkoli_be20@thapar.edu>
2024-07-16 10:57:38 +0530

    removed BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-      </Routes>
-    </Router>
+    <Home></Home>
   );
 };
 
